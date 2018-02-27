9e098d37f4408b5316e46ae6eaf7c468cb0c6978
Troy W <pyrodie18@gmail.com>
2018-02-26 21:21:46 -0500

    added sudo commands and formatting
    # 1.0.1  2/26/18
    #       Added additional commands to sudo groups
    #       Basic formatting updates

--- IPAServerBuild.sh
+++ IPAServerBuild.sh
@@ -2,122 +2,127 @@
 #  This is an automated BASH script to build an IPA server from a clean Centos 7.0 install.  It requires access to a functioning repo server.
 #  Written By:  Troy Ward
 #  Updated 2/21/18
-#  Version 1.0
+#  Version 1.0.1
+#
+# Change Log:
+#
+# 1.0.1  2/26/18
+#       Added additional commands to sudo groups
+#       Basic formatting updates
 
 
 ChangeIP () {
-	#Function to change the IP address of the box
-	FILES=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
+    #Function to change the IP address of the box
+    FILES=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
 
-  echo "Please enter the primary network interface from this list"
-  echo $FILES
-  echo ""
-  read FILE
+    echo "Please enter the primary network interface from this list"
+    echo $FILES
+    echo ""
+    read FILE
 
-  #Check to make sure the file exists
-  if [ ! -f /etc/sysconfig/network-scripts/"$FILE" ]; then
+    #Check to make sure the file exists
+    if [ ! -f /etc/sysconfig/network-scripts/"$FILE" ]; then
     echo "You did not enter a valid file name.  Exiting program"
     exit 2
-  fi
+    fi
 
-  #Get ip address info
-  echo "What IP address would you like to configure?"
-  read IPADDR
-  echo "What is the subnet mask?"
-  read NETMASK
-  echo "What is the default gateway?"
-  read GATEWAY
+    #Get ip address info
+    echo "What IP address would you like to configure?"
+    read IPADDR
+    echo "What is the subnet mask?"
+    read NETMASK
+    echo "What is the default gateway?"
+    read GATEWAY
 
+    #SET IP ADDRESS
+    sed -i '/BOOTPROTO/c\BOOTPROTO="static"' /etc/sysconfig/network-scripts/$FILE
+    sed -i '/ONBOOT/c\ONBOOT="yes"' /etc/sysconfig/network-scripts/$FILE
 
-  #SET IP ADDRESS
-  sed -i '/BOOTPROTO/c\BOOTPROTO="static"' /etc/sysconfig/network-scripts/$FILE
-  sed -i '/ONBOOT/c\ONBOOT="yes"' /etc/sysconfig/network-scripts/$FILE
-	
-	echo "Setting IP Address to $IPADDR"
-	if [ $(grep GATEWAY /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
-		sed -i "/GATEWAY/c\GATEWAY=$GATEWAY" /etc/sysconfig/network-scripts/$FILE
-	else
-		echo "GATEWAY=$GATEWAY" >> /etc/sysconfig/network-scripts/$FILE
-	fi
-	
-	echo "Setting gateway to $GATEWAY"
-	if [ $(grep IPADDR /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
-		sed -i "/IPADDR/c\IPADDR=$IPADDR" /etc/sysconfig/network-scripts/$FILE
-	else
-		echo "IPADDR=$IPADDR" >> /etc/sysconfig/network-scripts/$FILE
-	fi
+    echo "Setting IP Address to $IPADDR"
+    if [ $(grep GATEWAY /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
+        sed -i "/GATEWAY/c\GATEWAY=$GATEWAY" /etc/sysconfig/network-scripts/$FILE
+    else
+        echo "GATEWAY=$GATEWAY" >> /etc/sysconfig/network-scripts/$FILE
+    fi
 
-	echo "Setting netmask to $NETMASK"
-	if [ $(grep NETMASK /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
-		sed -i "/NETMASK/c\NETMASK=$NETMASK" /etc/sysconfig/network-scripts/$FILE
-	else
-		echo "NETMASK=$MASK" >> /etc/sysconfig/network-scripts/$FILE
-	fi
-	if [ $(grep DNS1 /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
-		sed -i '/DNS1/c\DNS1="192.168.122.1"' /etc/sysconfig/network-scripts/$FILE
-		#sed -i '/DNS1/c\DNS1="$IPADDR"' /etc/sysconfig/network-scripts/$FILE
-	else
-		echo "DNS1=$IPADDR" >> /etc/sysconfig/network-scripts/$FILE
-		#echo "DNS1=192.168.122.1" >> /etc/sysconfig/network-scripts/$FILE
-	fi
+    echo "Setting gateway to $GATEWAY"
+    if [ $(grep IPADDR /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
+        sed -i "/IPADDR/c\IPADDR=$IPADDR" /etc/sysconfig/network-scripts/$FILE
+    else
+        echo "IPADDR=$IPADDR" >> /etc/sysconfig/network-scripts/$FILE
+    fi
 
-  #RESTART NETWORKING SERVICES
-  echo "Restarting Networking Services"
-  #systemctl restart network
+    echo "Setting netmask to $NETMASK"
+    if [ $(grep NETMASK /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
+        sed -i "/NETMASK/c\NETMASK=$NETMASK" /etc/sysconfig/network-scripts/$FILE
+    else
+        echo "NETMASK=$MASK" >> /etc/sysconfig/network-scripts/$FILE
+    fi
+    if [ $(grep DNS1 /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
+        sed -i '/DNS1/c\DNS1="192.168.122.1"' /etc/sysconfig/network-scripts/$FILE
+        #sed -i '/DNS1/c\DNS1="$IPADDR"' /etc/sysconfig/network-scripts/$FILE
+    else
+        echo "DNS1=$IPADDR" >> /etc/sysconfig/network-scripts/$FILE
+        #echo "DNS1=192.168.122.1" >> /etc/sysconfig/network-scripts/$FILE
+    fi
+
+    #RESTART NETWORKING SERVICES
+    echo "Restarting Networking Services"
+    #systemctl restart network
 }
 
 #update repos
 yum update -y
-
 yum install -y vim net-tools ipa-server bind-dyndp-ldap ipa-server-dns rng-tools
 
 #Check IP Config
 FILES=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
 ADDRSET="FALSE"
 for FILE in $FILES; do
-  if [ $(grep static /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
-    #STATIC CONFIG FOUND, PULL OUT THE INFO AND SEE IF ITS CORRECT
-		n='[0-9]\{1,3\}'
-		IPADDR=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep IPADDR | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
-		NETMASK=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep NETMASK | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
-		GATEWAY=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep GATEWAY | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
-		DNS=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep DNS1 | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
+    if [ $(grep static /etc/sysconfig/network-scripts/$FILE | wc -l) == 1 ]; then
+    
+        #STATIC CONFIG FOUND, PULL OUT THE INFO AND SEE IF ITS CORRECT
+        n='[0-9]\{1,3\}'
+        IPADDR=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep IPADDR | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
+        NETMASK=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep NETMASK | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
+        GATEWAY=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep GATEWAY | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
+        DNS=$(cat /etc/sysconfig/network-scripts/$FILES | sed 's/\"//g' |grep DNS1 | sed "s/.*\=\($n\.$n\.$n\.$n\).*/\1/")
 
-		echo "Interface $FILE currently has the following configuration"
-		echo "IP Address:  $IPADDR"
-		echo "Subnet Mask:  $NETMASK"
-		echo "Gateway:  $GATEWAY"
-		echo "DNS:  $DNS"
-		echo
-		echo "Is this correct?"
-		read USERINPUT
+        echo "Interface $FILE currently has the following configuration"
+        echo "IP Address:  $IPADDR"
+        echo "Subnet Mask:  $NETMASK"
+        echo "Gateway:  $GATEWAY"
+        echo "DNS:  $DNS"
+        echo
+        echo "Is this correct?"
+        read USERINPUT
 
-		if [[ $USERINPUT == "y"* ]] || [[ $USERINPUT == "Y"* ]]; then
-                	ADDRSET="TRUE"
-		fi
-  fi
+        if [[ $USERINPUT == "y"* ]] || [[ $USERINPUT == "Y"* ]]; then
+            ADDRSET="TRUE"
+        fi
+    fi
 done
 
 if [ $ADDRSET == "FALSE" ]; then
-  echo "This VM does not currently have a static IP address configured"
-	ChangeIP
+    echo "This VM does not currently have a static IP address configured"
+    ChangeIP
 fi
 
 #CONFIGURE HOSTNAME
 echo "Please enter your domain [warroom2.kit]"
 read USERINPUT
 if [ -z $USERINPUT ]; then
-	DOMAIN="warroom2.kit"
+    DOMAIN="warroom2.kit"
 else
-	DOMAIN=$USERINPUT
+    DOMAIN=$USERINPUT
 fi
 
 echo "Please enter your computer name without domain [auth]:"
 read USERINPUT
 if [ -z $USERINPUT ]; then
-	COMPUTERNAME="auth"
+    COMPUTERNAME="auth"
 else
-	COMPUTERNAME=$USERINPUT
+    COMPUTERNAME=$USERINPUT
 fi
 
 DOMAINNAME="$COMPUTERNAME.$DOMAIN"
@@ -137,6 +142,7 @@ firewall-cmd --reload
 echo "Generating Randomness for IPA certs....This will take a min."
 cat /dev/urandom | rngtest -c 500
 
+# GENERATE ADMIN PASSWORDS AND INSTALL IPA SERVER
 DMPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&' | fold -w 15 | head -n 1)
 ADMINPASSWORD==$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&' | fold -w 15 | head -n 1)
 REALM=$(echo $DOMAIN | tr a-z A-Z)
@@ -182,12 +188,22 @@ ipa sudocmd-add /usr/sbin/service --desc="For starting/stopping services"
 ipa sudocmd-add /usr/sbin/shutdown --desc="Shutdown a system"
 ipa sudocmd-add /usr/sbin/reboot --desc="Reboot a system"
 ipa sudocmd-add /usr/sbin/ifconfig --desc="Configure IP Address"
+#TCPdump
+#Bro
+#Surricata
+#Snort
+#netsniff
+#nmap
+
 
 #Add sudo groups
 ipa sudocmdgroup-add sgrp-view-files --desc="View files commands"
 ipa sudocmdgroup-add sgrp-edit-files --desc="Edit file commands"
 ipa sudocmdgroup-add sgrp-system-restart --desc="System restart commands"
 ipa sudocmdgroup-add sgrp-services --desc="Commands to manage services"
+ipa sudocmdgroup-add sgrp-IDS --desc="IDS commands"
+ipa sudocmdgroup-add sgrp-packet-capture --desc="Packet Capture"
+ipa sudocmdgroup-add sgrp-enumeration --desc="Enumeration Tools"
 
 #Put sudo commands in groups
 ipa sudocmdgroup-add-member sgrp-view-files --sudocmds "/usr/bin/less"
@@ -208,15 +224,18 @@ ipa sudorule-add-allow-command srule-analyst-laptop --sudocmds=/usr/sbin/ifconfi
 ipa sudorule-add-host srule-analyst-laptop --hostgroups=ANL-Workstation
 ipa sudorule-add-user srule-analyst-laptop --groups=Analyst
 ipa sudorule-add-runasuser srule-analyst-laptop --users=root
+
 #Configure srule-admin-sensor
 ipa sudorule-add srule-admin-sensor --desc="Sensor admin rules on sensors (allow all)" --cmdcat='all'
 ipa sudorule-add-host srule-admin-sensor --hostgroups=NET-Sensor
 ipa sudorule-add-user srule-admin-sensor --groups=Sensor-Admin
 ipa sudorule-add-runasuser srule-admin-sensor --users=root
+
 #Configure srule-admin-inf
 ipa sudorule-add srule-admin-inf --desc="Infastructure admin rules on all devices (allow all)" --cmdcat='all' --hostcat='all'
 ipa sudorule-add-user srule-admin-inf --groups=INF-Admin
 ipa sudorule-add-runasuser srule-admin-inf --users=root
+
 #Configure srule-admin-splunk
 ipa sudorule-add srule-admin-splunk --desc="Splunk admin rules on splunk machines (allow all)" --cmdcat='all'
 ipa sudorule-add-host srule-admin-splunk --hostgroups=Splunk
